fa5404784f5dc23c8ce35e2611f0abdb85d67ee3
masha-pmrv <masha.pomorova.kpi@gmail.com>
2021-12-09 23:23:26 +0200

    lab1

--- calculator.cpp
+++ calculator.cpp
@@ -9,3 +9,7 @@ int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+
+int Calculator::Mul (double a, double b) {
+    return a * b;
+}
--- calculator.h
+++ calculator.h
@@ -4,7 +4,7 @@
 class Calculator
 {
     public:
-        int Add (int, int);
+        int Add (double, double);
         int Mul (double, double);
 };
 
